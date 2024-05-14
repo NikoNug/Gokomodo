b4d040b943474f005aa579c0bcae2ffd0114b0b9
NikoNug <nikonugraha03@gmail.com>
2024-05-14 17:06:26 +0700

    Init Project & DB

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowMethods: strings.Join([]string{
+			fiber.MethodGet,
+			fiber.MethodPost,
+			fiber.MethodPatch,
+			fiber.MethodPut,
+			fiber.MethodDelete,
+		}, "*"),
+	}))
+
+}
